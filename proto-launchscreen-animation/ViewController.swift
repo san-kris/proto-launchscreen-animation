ec2b333ed98e8cf571c82030bcccd7fe2d2285ba
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-05 14:09:37 -0600

    Initial Commit

---
+++ proto-launchscreen-animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto-launchscreen-animation
+//
+//  Created by Santosh Krishnamurthy on 3/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
