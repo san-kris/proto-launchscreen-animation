df466d00ba3c0d73111f58ffe793b16be6d3f5e0
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-06 11:35:31 -0600

    Load new ViewController after animation. Use a lazy var declaration along with constructor to ensure new VC is only presented once

--- proto-launchscreen-animation/LaunchAnimationViewController.swift
+++ proto-launchscreen-animation/LaunchAnimationViewController.swift
@@ -19,22 +19,38 @@ class LaunchAnimationViewController: UIViewController {
     
     // Create a image view with logo
     private let imageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 124, height: 124))
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 128, height: 128))
         imageView.image = UIImage(named: "launch-image")
         return imageView
     }()
     
+    // If you want a function to execute only once no matter how many times its called
+    // Declate a lazy var of type Void
+    // Making the varibale lazy ensures that the constructor is not called when VC is loaded
+    private lazy var loadHomeVC: Void = {
+        // This is a constructor that is called once when the variable is instantiated
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+            let viewController = HomeViewController()
+            viewController.modalTransitionStyle = .crossDissolve
+            viewController.modalPresentationStyle = .fullScreen
+            self.present(viewController, animated: true)
+
+        })
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("in ViewDidLoad")
         // Do any additional setup after loading the view.
         view.backgroundColor = .green
-        // Add imageview as a subview
+        // Add imageview as a subview.
+        // Adding a subview or changing the size / rect of view causes the method viewDidLayoutSubviews to be called
         view.addSubview(imageView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+        print("in ViewDidLayoutSubviews")
         imageView.center = view.center
         // run animation code with .5 sec delay
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+0.5, execute: {
@@ -45,20 +61,33 @@ class LaunchAnimationViewController: UIViewController {
     }
     
     private func animate(){
+        print("animate called")
         UIView.animate(withDuration: 1, animations: {
-            let size = self.view.frame.size.width * 1.5
+            let size = self.view.frame.size.width * 3
             let diffx = size - self.view.frame.size.width
             let diffy = self.view.frame.size.height - size
             
+            // Changing the size / rect of a view causes function "viewDidLayoutSubviews" to be called again
             self.imageView.frame = CGRect(
                 x: -(diffx/2),
                 y: diffy/2,
                 width: size,
                 height: size
             )
+                        
         }) { flag in
-            print("animation completed")
+            print("scale animation completed: \(flag)")
         }
+        
+        UIView.animate(withDuration: 1.5, animations: {
+            self.imageView.alpha = 0
+        }) { flag in
+            print("fade animation completed: \(flag)")
+            // at the time of this assignment, the variable loadHomeVC is instantiated lazily and the constructor is called.
+            // using the same variable again, does not call the constructor
+            _ = self.loadHomeVC
+        }
+        
     }
 
 
